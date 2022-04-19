e7ab783d514892c7672e30ace8d5edd127bcc840
SamTredgett <sam.tredgett@gmail.com>
2022-04-19 08:49:34 +0100

    first commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCApp.Models;
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel doggo = new() { Name = "Henry", Age = 1 };
+            return View(doggo);
+        }
+
+        public IActionResult Hello()
+        {
+            DogViewModel otherdoggo = new() { Name = "Jeremiah", Age = 3 };
+            return View(otherdoggo);
+        }
+        
+        public IActionResult Create()
+        {
+            return View();  
+        }
+    }
+}
